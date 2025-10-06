3363880bc3f50c8e88a581ac47dd432e99920aff
Bhargav K L <klbhargav3@gmail.com>
2025-10-06 10:12:32 +0530

    urls.py

--- aiapp/urls.py
+++ aiapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("",views.login_view,name="login"),
     path("signup/",views.signup_view,name="signup"),
     path("chat/",views.chat_view,name="chat"),
-    path("logout/",views.login_view,name="logout"),
+    path("logout/",views.logout_view,name="logout"),
     path("history/",views.chat_history,name="history"),
     path("history/<int:id>/",views.specific_history,name="specific")
     ]
